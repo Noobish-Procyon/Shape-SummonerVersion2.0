44af0ed2bac63004dae67525f6436b25ada12846
Noobish-Procyon <justin.mo313@education.nsw.gov.au>
2026-07-24 16:36:32 +1000

    Create classEvo.js

---
+++ classEvo.js
@@ -0,0 +1,50 @@
+// classEvo.js
+
+const classEvo = {
+  circle: ["circle", "orb", "nova"],
+  square: ["square", "fortress", "citadel"],
+  triangle: ["triangle", "prism", "laserCore"],
+  pentagram: ["pentagram", "hellstar", "demonSun"],
+  octagon: ["octagon", "hive", "swarmLord"],
+  hexagon: ["hexagon", "vampire", "bloodGod"]
+};
+
+const evoPrices = {
+  orb: 200,
+  nova: 500,
+
+  fortress: 300,
+  citadel: 700,
+
+  prism: 350,
+  laserCore: 800,
+
+  hellstar: 400,
+  demonSun: 900,
+
+  hive: 450,
+  swarmLord: 1000,
+
+  vampire: 350,
+  bloodGod: 900
+};
+
+const evoColors = {
+  orb: "#88ccff",
+  nova: "#cce6ff",
+
+  fortress: "#3366ff",
+  citadel: "#0033cc",
+
+  prism: "#cc33ff",
+  laserCore: "#ff00ff",
+
+  hellstar: "#ff3333",
+  demonSun: "#990000",
+
+  hive: "#33ff99",
+  swarmLord: "#00cc66",
+
+  vampire: "#ff3366",
+  bloodGod: "#cc0033"
+};
